c37cab2e31099856185f36138a0debfd2675ece8
Vinc <spielermr05@gmail.com>
2025-06-14 03:58:52 +0200

    Added F addition and Bug fixes
    [+] Simple display for new Mechanic
    [f] Fixed Exit button
    [+] Added Water animation
    [+] Added Firefly to Level1

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,7 +12,7 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        StartCoroutine(waitBeforeQuit());
+        Application.Quit();
     }
 
     IEnumerator waitBeforeQuit()
